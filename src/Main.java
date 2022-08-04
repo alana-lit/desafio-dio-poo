8fdfea6e59144aa4fd5251b93bb5e99cd330b470
Alana Lima Tavares <alanapfss@gmail.com>
2022-08-04 10:31:35 -0300

    Finalização da abstração de um bootcamp

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -16,13 +18,32 @@ public class Main {
         cursoJS.setDescricao("Descricaso curso JavaScript");
         cursoJS.setCargaHoraria(8);
 
-        Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("Mentoria de Java");
-        mentoria.setDescricao("Descricao mentoria");
-        mentoria.setData(LocalDate.now());
+        Mentoria mentoriaJava = new Mentoria();
+        mentoriaJava.setTitulo("Mentoria de Java");
+        mentoriaJava.setDescricao("Descricao mentoria");
+        mentoriaJava.setData(LocalDate.now());
 
-        System.out.println(cursoJava.toString());
-        System.out.println(cursoJS);
-        System.out.println(mentoria);
+//        System.out.println(cursoJava.toString());
+//        System.out.println(cursoJS);
+//        System.out.println(mentoriaJava);
+
+        Bootcamp bootcampJava = new Bootcamp();
+        bootcampJava.setNome("Java Developer");
+        bootcampJava.setDescricao("Bootcamp de Java");
+        bootcampJava.getConteudos().add(cursoJava);
+        bootcampJava.getConteudos().add(mentoriaJava);
+
+        Dev devAlana = new Dev();
+        devAlana.setNome("Alana");
+        devAlana.inscreverBootcamp(bootcampJava);
+        System.out.println("Conteúdos Inscritos de "+devAlana.getNome()+": "+devAlana.getConteudosInscritos());
+        devAlana.progredir();
+        devAlana.progredir();
+        System.out.println("-----------------------");
+        System.out.println("Conteúdos Concluidos de "+devAlana.getNome()+": "+devAlana.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos de "+devAlana.getNome()+": "+devAlana.getConteudosInscritos());
+        System.out.println("XP: "+devAlana.calcularTotalXP());
+
+
     }
 }
